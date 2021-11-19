4228a3d2edf9529f3752ca071581ad86ac85ab7f
DxTea <81072307+DxTea@users.noreply.github.com>
2021-11-20 03:35:29 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import Games.GameChoice;
+
 /**
  * основной класс для запуска игры
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new GameChoice().starting();
     }
 }
