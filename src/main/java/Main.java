c3b27c9a80b48d5e3dd8ef6cd0b28a1d5a0d830b
DxTea <dexter_010@mail.ru>
2021-12-14 20:40:28 +0500

    solving BIG problems and update task 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 import Menu.ConsoleBotController;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 /**
  * основной класс
  */
 public class Main {
     public static void main(String[] args) {
-        new ConsoleBotController().start();
+        //new ConsoleBotController().start();
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
